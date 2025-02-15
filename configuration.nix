f3d4a102ee6993fda3213ed958c1aceced9f2e61
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-02-15 13:33:41 +0200

    Fix bootloader

--- configuration.nix
+++ configuration.nix
@@ -11,9 +11,24 @@
     ];
 
   # Bootloader.
+  boot.loader.systemd-boot.enable = false;
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "nodev";
   boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.extraEntries = ''
+    menuentry "Reboot" {
+      reboot
+    }
+    menuentry "Poweroff" {
+      halt
+    }
+  '';
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
