e85d40030d4565d3e05233b722e01f69d5cce254
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-02-24 23:08:01 +0200

    Update waybar config

--- programs/waybar/default.nix
+++ programs/waybar/default.nix
@@ -1,4 +1,5 @@
 {config, ...}: {
   home.file.".config/waybar/config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/waybar/config";
   home.file.".config/waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/waybar/style.css";
+  home.file.".config/waybar/modules.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/waybar/modules.json";
 }
