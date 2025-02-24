da486a1d9ebcfdeb2fadd2d7d7055f8eb94c674d
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-02-24 14:55:44 +0200

    Update kitty config

--- programs/kitty/default.nix
+++ programs/kitty/default.nix
@@ -1,4 +1,4 @@
 { config, lib, ... }: {
 
-	home.file.".config/kitty/kitty.conf".source = lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/kitty/kitty.conf";
+  home.file.".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/kitty/kitty.conf";
 }
