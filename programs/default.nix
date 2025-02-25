0fe405f9397b886bd7b6157bc3a70b8e3ab05c45
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-02-25 16:05:16 +0200

    Add wlogout config

--- programs/default.nix
+++ programs/default.nix
@@ -11,5 +11,6 @@
 
   # copy nvim directory to .config/nvim
   xdg.configFile.nvim.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/nvim";
+  xdg.configFile.wlogout.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/wlogout";
   home.file.".config/wal".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/wal";
 }
