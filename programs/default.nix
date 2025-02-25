941178045504b920187291745316abfcf300b654
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-02-25 22:44:56 +0200

    Add rofi config

--- programs/default.nix
+++ programs/default.nix
@@ -12,5 +12,6 @@
   # copy nvim directory to .config/nvim
   xdg.configFile.nvim.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/nvim";
   xdg.configFile.wlogout.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/wlogout";
+  xdg.configFile.rofi.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/rofi";
   home.file.".config/wal".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/programs/wal";
 }
