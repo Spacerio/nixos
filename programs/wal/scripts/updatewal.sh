edb07aa1d2ac1bc4c257675f0eda8a4021a724ca
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-02-25 18:36:25 +0200

    update pywal config

--- programs/wal/scripts/updatewal.sh
+++ programs/wal/scripts/updatewal.sh
@@ -4,11 +4,12 @@ img=$(find ~/.dotfiles/imgs -type f | wofi -d)
 
 echo "$img"
 
-wal -qsi "$img"
+wal -qi "$img"
 
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar/
+
+cp "$img" ~/.cache/wallpaper
 
 swww img "$img"
  
 pkill waybar; waybar &
-
