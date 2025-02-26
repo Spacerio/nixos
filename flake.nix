4f3980fc9ac55d8c2f1219c917eb353aadb96ca2
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-02-27 00:48:01 +0200

    Add spicetify

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+	spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+	spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
