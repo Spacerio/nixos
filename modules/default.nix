7bf31696c4f9adb54c6a4ea4c1f89316cef1e00b
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-03-05 22:44:36 +0200

    Move general configs to dotfiles directory

--- modules/default.nix
+++ modules/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ./sway
     ./vim
-    ./hyprland
     ./waybar
     ./tmux
     ./terminal/fish.nix
@@ -11,7 +10,8 @@
 
   # copy nvim directory to .config/nvim
   xdg.configFile.nvim.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nvim";
-  xdg.configFile.wlogout.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/modules/wlogout";
-  xdg.configFile.rofi.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/modules/rofi";
-  home.file.".config/wal".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nixos/modules/wal";
+  xdg.configFile.wlogout.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/wlogout";
+  xdg.configFile.rofi.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/rofi";
+  xdg.configFile.hypr.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/hypr";
+  xdg.configFile.wal.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/wal";
 }
