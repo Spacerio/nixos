3e5f975eca87cad4e15439f7e1ee2c72ad0d1d5e
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-03-14 22:54:14 +0200

    Update fish config with fzf completion

--- modules/terminal/fish.nix
+++ modules/terminal/fish.nix
@@ -54,6 +54,17 @@ in {
 	    test -n "$file" && nvim $file
 
 	  '';
+
+      fzf_complete = ''
+        set -l cmdline (commandline)
+        # HACK: Color descriptions manually.
+        complete -C | string replace -r \t'(.*)$' \t(set_color $fish_pager_color_description)'$1'(set_color normal) \
+          | fzf -d \t -1 -0 --ansi --header="$cmdline" --height="80%" --tabstop=4 \
+          | read -l token
+        # Remove description
+        set token (string replace -r \t'.*' ''' -- $token)
+        commandline -rt "$token"
+        '';
     };
     interactiveShellInit = ''
       # if command -q tmux && [ "$TERM" != "tmux" ] && [ -z "$TMUX" ]
@@ -67,6 +78,7 @@ in {
 
 	  bind \en 'nvim -c ":lua Snacks.picker.smart()"'  
 	  bind \ed 've'  
+      bind \eu 'fzf_complete; commandline -f repaint'
 
       set -gx fish_greeting
       set -gx EDITOR nvim
