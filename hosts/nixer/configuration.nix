aa1e069cd128e4695464c5b486462c6b998e3b46
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-04-08 20:38:22 +0300

    Enable static ip for faster boot times

--- hosts/nixer/configuration.nix
+++ hosts/nixer/configuration.nix
@@ -266,21 +266,21 @@
   # networking.firewall.enable = false;
 
   networking = {
-    networkmanager.enable = true;
-    # # not working, need to figure out atleast subnets
-    # dhcpcd.enable = false;
-    # enableIPv6 = false;
-    # nameservers = [ "8.8.8.8" "0.0.0.0" ];
-    # interfaces.enp4s0 = {
-    #   ipv4.addresses = [{
-    # 	address = "91.154.72.10";
-    # 	prefixLength = 24;
-    #   }];
-    # };
-    # defaultGateway = {
-    #   address = "192.0.2.1";
-    #   interface = "enp4s0";
-    # };
+    defaultGateway = {
+      address = "91.154.72.1";
+      interface = "enp4s0";
+    };
+    interfaces.enp4s0 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+        address = "91.154.72.10";
+        prefixLength = 23;
+      }];
+    };
+    networkmanager.enable = false;
+    dhcpcd.enable = false;
+    enableIPv6 = false;
+    nameservers = [ "8.8.8.8" "0.0.0.0" ];
   };
 
   #  systemd.network.enable = true;
